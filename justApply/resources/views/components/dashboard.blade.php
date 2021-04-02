b7ef6be55d10c6a26f230fc48e3514d1a4e5531d
Alexander <gwartneydev@gmail.com>
2021-04-01 21:04:11 -0700

    JUS17 added in dashboard template
    Began working on the main dashboard styling and components.

---
+++ justApply/resources/views/components/dashboard.blade.php
@@ -0,0 +1,42 @@
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <form type>
+        <h2 id= jobcardinputForm>Add jobs to apply to</h2>
+        <div class="mb-3">
+         
+          <input type="input" class="form-control" id="exampleFormControlInput1" placeholder="JobName">
+        
+          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Job Role">
+      
+          <input type="input" class="form-control" id="exampleFormControlInput1" placeholder="Job Link">
+    
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+     
+         <button type="submit" value="Submit" class="btn btn-primary btn-lg">Large button</button>
+
+      </div>
+      </form>
+   </div>
+
+   
+   <div class="col">
+               <div class="row">
+                     <div class="col">
+                        <h5 class="card-title">Special title treatment</h5>
+                     </div>
+                     <div class="col">
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                     </div>
+               </div>
+
+
+
+      </div>
+</div>
+
+
+
+
+
--- justApply/routes/web.php
+++ justApply/routes/web.php
@@ -14,11 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboardMain');
 });
 
-Route::get('/FaviroteJobs');
-
-Route::get('/JobsApliedTo');
-
-Route::get('php artisan breeze:install');
